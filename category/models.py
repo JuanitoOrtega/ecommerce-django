5952c352a1975ffeaaa49fe0b69e80c551320156
Juanito Ortega <ortegaj83@gmail.com>
2022-08-11 02:11:55 -0400

    first commit: categories links

---
+++ category/models.py
@@ -0,0 +1,21 @@
+from audioop import reverse
+from distutils.command import upload
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+  category_name = models.CharField('Categoría', max_length=30, unique=True)
+  description = models.CharField('Descripción', max_length=255, blank=True)
+  slug = models.CharField(max_length=100, unique=True)
+  cat_image = models.ImageField('Imagen', upload_to='photos/categories', blank=True)
+
+  class Meta:
+    verbose_name = 'categoría'
+    verbose_name_plural = 'categorías'
+
+  def get_url(self):
+    return reverse('products_by_category', args=[self.slug])
+
+  def __str__(self):
+    return self.category_name
---
+++ store/models.py
@@ -0,0 +1,21 @@
+from email.policy import default
+from enum import unique
+from django.db import models
+
+from category.models import Category
+
+# Create your models here.
+class Product(models.Model):
+  product_name = models.CharField('Nombre del producto', max_length=200, unique=True)
+  slug = models.CharField(max_length=200, unique=True)
+  description = models.TextField('Descripción', max_length=500, blank=True)
+  price = models.DecimalField('Precio', max_digits=6, decimal_places=2)
+  images = models.ImageField('Images', upload_to='photos/products')
+  stock = models.IntegerField('Cantidad disponible')
+  is_available = models.BooleanField('Disponible', default=True)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  created_date = models.DateTimeField('Fecha de creación', auto_now_add=True)
+  modified_date = models.DateTimeField('Última actualización', auto_now=True)
+
+  def __str__(self):
+    return self.product_name
