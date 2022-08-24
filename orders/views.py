20773f1c9045d8ed8b76dfd1eee37f7dbd412059
Juanito Ortega <ortegaj83@gmail.com>
2022-08-24 06:52:05 -0400

    order de products

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from carts.models import CartItem
 from .forms import OrderForm
-from .models import Order
 import datetime
-from .models import Order, Payment
+from .models import Order, Payment, OrderProduct
 import json
+from store.models import Product
 
 # Create your views here.
 def payments(request):
@@ -23,6 +23,35 @@ def payments(request):
   order.payment = payment
   order.is_ordered = True
   order.save()
+
+  # Mover todos los carrito items hacia la tabla order product
+  cart_items = CartItem.objects.filter(user=request.user)
+
+  for item in cart_items:
+    orderproduct = OrderProduct()
+    orderproduct.order_id = order.id
+    orderproduct.payment = payment
+    orderproduct.user_id = request.user.id
+    orderproduct.product_id = item.product_id
+    orderproduct.quantity = item.quantity
+    orderproduct.product_price = item.product.price
+    orderproduct.ordered = True
+    orderproduct.save()
+
+    # Agrega variaciones a la orden del producto
+    cart_item = CartItem.objects.get(id=item.id)
+    product_variation = cart_item.variations.all()
+    orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+    orderproduct.variation.set(product_variation)
+    orderproduct.save()
+
+    # Actualizar stock y carrito de compras
+    product = Product.objects.get(id=item.product_id)
+    product.stock -= item.quantity
+    product.save()
+
+    # Limpiamos el carrito despues de que el cliente ordene la compra
+    CartItem.objects.filter(user=request.user).delete()
   
   return render(request, 'orders/payments.html')
 
